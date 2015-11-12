7debd6cdc5f90830a2ee95355d99e0f60649e022
Juan Roldan <juanroldan1989@gmail.com>
2015-11-12 01:00:18 -0300

    Schema bump

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112030733) do
+ActiveRecord::Schema.define(version: 20151112035412) do
 
   create_table "posts", force: true do |t|
     t.integer  "user_id"
@@ -20,5 +20,17 @@ ActiveRecord::Schema.define(version: 20151112030733) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "recipes", force: true do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "recipes", ["user_id"], name: "index_recipes_on_user_id"
 
 end
