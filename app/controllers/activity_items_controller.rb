3f0ec8d84aaa541509dd8307859c71912d01f646
Juan Roldan <juanroldan1989@gmail.com>
2015-11-12 23:00:38 -0300

    Activity feed only returning latest 20 items

--- app/controllers/activity_items_controller.rb
+++ app/controllers/activity_items_controller.rb
@@ -3,7 +3,7 @@ class ActivityItemsController < ApplicationController
   # GET /activy_items
   # GET /activy_items.json
   def index
-    @activity_items = ActivityItem.all
+    @activity_items = ActivityItem.latest(20)
 
     render json: @activy_items
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :activity_items, except: [:new, :edit]
+  resources :activity_items, only: [:index]
   resources :reviews, except: [:new, :edit]
   resources :recipes, except: [:new, :edit]
   resources :posts, except: [:new, :edit]
