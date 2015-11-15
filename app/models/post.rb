6317e90a172f53e2908b58f7a5ac069383cd56da
Juan Roldan <juanroldan1989@gmail.com>
2015-11-15 02:11:47 -0300

    Post background jobs triggered on create/update/delete actions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+
+  private
+
+  after_commit :generate_activity_item_on_create,  on: :create
+  after_commit :generate_activity_item_on_update,  on: :update
+  after_commit :generate_activity_item_on_destroy, on: :destroy
+
+  def generate_activity_item_on_create
+    ActivityItemWorker.new("create", self.id, "post", title, user_id).perform
+  end
+
+  def generate_activity_item_on_update
+    ActivityItemWorker.new("update", self.id, "post", title, user_id).perform
+  end
+
+  def generate_activity_item_on_destroy
+    ActivityItemWorker.new("delete", self.id, "post", title, user_id).perform
+  end
 end
