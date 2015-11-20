8b32236641c7fdd88333c4311916a3cd749b4fb9
Juan Roldan <juanroldan1989@gmail.com>
2015-11-20 06:29:44 +0000

    API version set to "v1"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :activity_items, only: [:index]
-  resources :reviews, except: [:new, :edit]
-  resources :recipes, except: [:new, :edit]
-  resources :posts, except: [:new, :edit]
+  namespace :v1 do
+    resources :activity_items, only: [:index]
+    resources :reviews, except: [:new, :edit]
+    resources :recipes, except: [:new, :edit]
+    resources :posts, except: [:new, :edit]
+  end
 end
