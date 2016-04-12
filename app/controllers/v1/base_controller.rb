fa0e1413bda8e8ec368f5f4b8f3b5e35d3362f29
Juan Roldan <juanroldan1989@gmail.com>
2016-04-12 23:19:30 +1200

    PostsController - Actions logic moved into common BaseController

---
+++ app/controllers/v1/base_controller.rb
@@ -0,0 +1,45 @@
+module V1
+  class BaseController < ApplicationController
+    before_action :set_resource, only: [:show, :update, :destroy]
+
+    def index
+      @resources = resource_class.all
+
+      render json: { resources: @resources }
+    end
+
+    def show
+      render json: @resource
+    end
+
+    def create
+      @resource = resource_class.new(resource_params)
+
+      if @resource.save
+        render json: @resource, status: :created
+      else
+        render json: @resource.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @resource.update(resource_params)
+        head :no_content
+      else
+        render json: @resource.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @resource.destroy
+
+      head :no_content
+    end
+
+    private
+
+    def set_resource
+      @resource = resource_class.find(params[:id])
+    end
+  end
+end
--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -1,61 +1,14 @@
 module V1
-  class PostsController < ApplicationController
-    before_action :set_post, only: [:show, :update, :destroy]
-
-    # GET /posts
-    # GET /posts.json
-    def index
-      @posts = Post.all
-
-      render json: { posts: @posts }
-    end
-
-    # GET /posts/1
-    # GET /posts/1.json
-    def show
-      render json: @post
-    end
-
-    # POST /posts
-    # POST /posts.json
-    def create
-      @post = Post.new(post_params)
-
-      if @post.save
-        render json: @post, status: :created
-      else
-        render json: @post.errors, status: :unprocessable_entity
-      end
-    end
-
-    # PATCH/PUT /posts/1
-    # PATCH/PUT /posts/1.json
-    def update
-      @post = Post.find(params[:id])
-
-      if @post.update(post_params)
-        head :no_content
-      else
-        render json: @post.errors, status: :unprocessable_entity
-      end
-    end
-
-    # DELETE /posts/1
-    # DELETE /posts/1.json
-    def destroy
-      @post.destroy
-
-      head :no_content
-    end
+  class PostsController < BaseController
 
     private
 
-      def set_post
-        @post = Post.find(params[:id])
-      end
+    def resource_params
+      params.require(:post).permit(:user_id, :title, :body)
+    end
 
-      def post_params
-        params.require(:post).permit(:user_id, :title, :body)
-      end
+    def resource_class
+      Post
+    end
   end
 end
